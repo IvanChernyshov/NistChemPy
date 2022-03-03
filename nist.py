5ae3ccf8ad662688bcfed58cc04dfda43b77de10
Grim <ivan-chernyshoff@yandex.ru>
2022-03-03 14:24:43 +0300

    Added search

--- nist.py
+++ nist.py
@@ -5,10 +5,35 @@ Python API for NIST Chemistry WebBook
 #%% Imports
 
 import re, os, requests
-from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup, Comment
 
 
-#%% Objects
+#%% Support functions
+
+def is_compound(soup):
+    '''
+    Checks if html is a single compound page and returns NIST ID if yes
+    '''
+    header = soup.findAll('h1', {'id': 'Top'})
+    if not header:
+        return None
+    # get info
+    header = header[0]
+    info = header.findNext('ul')
+    if not info:
+        return None
+    # extract NIST ID
+    for comment in soup.findAll(text = lambda text: isinstance(text, Comment)):
+        comment = str(comment).replace('\r\n', '').replace('\n', '')
+        if not '/cgi/cbook.cgi' in comment:
+            continue
+        return re.search(r'/cgi/cbook.cgi\?Form=(.*?)&', comment).group(1)
+    
+    return None
+
+
+#%% Compound-related classes
 
 class Spectrum():
     '''
@@ -36,10 +61,10 @@ class Spectrum():
             outf.write(self.jdx_text)
     
     def __str__(self):
-        return f'Compound({self.compound.ID}, {self._pretty_names[self.spec_type]} #{self.spec_idx})'
+        return f'Spectrum({self.compound.ID}, {self._pretty_names[self.spec_type]} #{self.spec_idx})'
     
     def __repr__(self):
-        return f'Compound({self.compound.ID}, {self._pretty_names[self.spec_type]} #{self.spec_idx})'
+        return f'Spectrum({self.compound.ID}, {self._pretty_names[self.spec_type]} #{self.spec_idx})'
 
 
 class Compound():
@@ -249,10 +274,173 @@ class Compound():
         return f'Compound({self.ID})'
 
 
+#%% Search-related classes
 
-#%% Main functions
+class SearchParameters():
+    '''
+    Object containing parameters for compound search in NIST WebBook
+    To get the full description of available options please use
+    "print_options" method
+    '''
+    info = {'Units': ('Units for thermodynamic data, "SI" or "CAL" for calorie-based', 'SI'),
+            'MatchIso': ('Exactly match the specified isotopes (formula search only)', False),
+            'AllowOther': ('Allow elements not specified in formula (formula search only)', False),
+            'AllowExtra': ('Allow more atoms of elements in formula than specified (formula search only)', False),
+            'NoIon': ('Exclude ions from the search (formula search only)', True),
+            'cTG': ('Contains gas-phase thermodynamic data', False),
+            'cTC': ('Contains condensed-phase thermodynamic data', False),
+            'cTP': ('Contains phase-change thermodynamic data', False),
+            'cTR': ('Contains reaction thermodynamic data', False),
+            'cIE': ('Contains ion energetics thermodynamic data', False),
+            'cIC': ('Contains ion cluster thermodynamic data', False),
+            'cIR': ('Contains IR data', False),
+            'cTZ': ('Contains THz IR data', False),
+            'cMS': ('Contains MS data', False),
+            'cUV': ('Contains UV/Vis data', False),
+            'cGC': ('Contains gas chromatography data', False),
+            'cES': ('Contains vibrational and electronic energy levels', False),
+            'cDI': ('Contains constants of diatomic molecules', False),
+            'cSO': ('Contains info on Henry\'s law', False)}
+    
+    def print_options(self):
+        '''
+        Prints available search options
+        '''
+        for key, val in self.info.items():
+            print(f'{key}: {val[0]}')
+    
+    def get_request_parameters(self):
+        '''
+        Returns dictionary with GET parameters
+        '''
+        params = {'Units': self.Units}
+        for key in self.info:
+            if key == 'Units':
+                continue
+            val = getattr(self, key)
+            if val:
+                params[key] = 'on'
+        
+        return params
+    
+    def __init__(self, **kwargs):
+        # set default
+        for key, val in self.info.items():
+            setattr(self, key, val[1])
+        # check kwargs
+        for key, val in kwargs.items():
+            if key not in self.info:
+                raise TypeError(f'"{key}" is an invalid keyword argument for SearchParameters')
+            if key == 'Units' and val not in ('SI', 'CAL'):
+                raise ValueError(f'Bad value for "Units" parameter: {val}')
+            if key != 'Units' and type(val) is not bool:
+                raise ValueError(f'Bad value for "{key}" parameter: {val}')
+            setattr(self, key, val)
+    
+    def __str__(self):
+        sep = ',\n' + ' '*17
+        text = [f'SearchParameters(Units={self.Units}'] + \
+               [f'{key}={getattr(self, key)}' for key in self.info if key != 'Units' and getattr(self, key)]
+        text[-1] = text[-1] + ')'
+        
+        return sep.join(text)
+    
+    def __repr__(self):
+        sep = ',\n' + ' '*17
+        text = [f'SearchParameters(Units={self.Units}'] + \
+               [f'{key}={getattr(self, key)}' for key in self.info if key != 'Units' and getattr(self, key)]
+        text[-1] = text[-1] + ')'
+        
+        return sep.join(text)
 
 
+class LostDataException(Exception):
+    '''
+    Exception for searches with > 400 of output results
+    '''
+    def __init__(self, message):
+        super().__init__(message)
 
+
+class Search():
+    '''
+    Object for searching in NIST Chemistry WebBook
+    '''
+    
+    # NIST URLs
+    _NIST_URL = 'https://webbook.nist.gov'
+    _COMP_ID = '/cgi/cbook.cgi'
+    
+    # parameters data
+    search_types = {'formula': 'Formula', 'name': 'Name', 'inchi': 'InChI', 'cas': 'ID'}
+    formula_only = ('MatchIso', 'AllowOther', 'AllowExtra', 'NoIon')
+    
+    def __init__(self, **kwargs):
+        self.parameters = SearchParameters(**kwargs)
+        self.IDs = []
+        self.compounds = []
+        self.lost = False
+        self.success = True
+    
+    def find_compounds(self, identifier, search_type, **kwargs):
+        '''
+        Search for species data by chemical name
+        search_type must be one of 'formula', 'name', 'inchi', 'cas'
+        clear_found: clear all found compounds
+        raise_lost: raise exception if limit of 400 compounds per search
+                    was achieved
+        '''
+        if search_type not in self.search_types:
+            raise ValueError(f'Bad search_type value: {search_type}')
+        # prepare GET parameters
+        params = {self.search_types[search_type]: identifier}
+        params.update(self.parameters.get_request_parameters())
+        addend = SearchParameters(**kwargs)
+        params.update(addend.get_request_parameters())
+        # load webpage
+        r = requests.get(self._NIST_URL + self._COMP_ID, params)
+        if not r.ok:
+            self.success = False
+            self.IDs = []
+            self.compounds = []
+            self.lost = False
+            return
+        soup = BeautifulSoup(re.sub('clss=', 'class=', r.text),
+                             features = 'html.parser')
+        # check if no compounds
+        err = 'no matching species found' if search_type == 'inchi' else 'not found'
+        if sum([err in _.text.lower() for _ in soup.findAll('h1')]):
+            self.success = True
+            self.IDs = []
+            self.compounds = []
+            self.lost = False
+            return
+        # check if one compound
+        flag = is_compound(soup)
+        if flag:
+            self.success = True
+            self.IDs = [flag]
+            self.compounds = []
+            self.lost = False
+            return
+        # extract IDs
+        refs = soup.find('ol').findChildren('a', href = re.compile(self._COMP_ID))
+        IDs = [parse_qs(urlparse(a.attrs['href']).query)['ID'][0] for a in refs]
+        self.IDs = IDs
+        self.compounds = []
+        self.success = True
+        self.lost = 'Due to the large number of matching species' in soup.text
+    
+    def load_found_compounds(self):
+        '''
+        Loads compounds
+        '''
+        self.compounds = [Compound(ID) for ID in self.IDs]
+    
+    def __str__(self):
+        return f'Search(Success={self.success}, Lost={self.lost}, Found={len(self.IDs)})'
+    
+    def __repr__(self):
+        return f'Search(Success={self.success}, Lost={self.lost}, Found={len(self.IDs)})'
 
 
