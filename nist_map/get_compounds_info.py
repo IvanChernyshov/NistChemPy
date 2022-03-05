ed17363a3b94fb0f826481ed2ee9f2a9d3a5c683
Grim <ivan-chernyshoff@yandex.ru>
2022-03-05 11:23:18 +0300

    Added full list of NIST compounds

---
+++ nist_map/get_compounds_info.py
@@ -0,0 +1,18 @@
+'''
+Downloads main info for all NIST Chemistry WebBook compounds
+'''
+
+#%% Imports
+
+import json
+import pandas as pd
+import nistchempy as nist
+
+
+#%% Load compounds
+
+
+
+
+
+
