b755e67b035c56907aa080bcaa8fab9da542e089
Grim <ivan-chernyshoff@yandex.ru>
2022-03-08 16:43:02 +0300

    Added NIST data to the package

---
+++ nistchempy/__init__.py
@@ -0,0 +1,10 @@
+# imports
+from nistchempy.nistchempy import *
+
+# module functions
+__all__ = [
+    'get_all_data',
+    'Compound', 'Spectrum'
+    'print_search_parameters', 'SearchParameters', 'Search'
+]
+
