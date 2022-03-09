f10204405b027c2114d927a1b055fc9cc9418db2
Grim <ivan-chernyshoff@yandex.ru>
2022-03-09 16:15:42 +0300

    Added version to the package

--- nistchempy/__init__.py
+++ nistchempy/__init__.py
@@ -1,10 +1,12 @@
 # imports
-from .nistchempy import get_all_data, Compound, Spectrum, \
+from .nistchempy import __version__, \
+                        get_all_data, Compound, Spectrum, \
                         print_search_parameters, \
                         SearchParameters, Search
 
 # module functions
 __all__ = [
+    '__version__',
     'get_all_data',
     'Compound', 'Spectrum'
     'print_search_parameters', 'SearchParameters', 'Search'
