6c5bcce7c62b95f7edca7312985ebdffc350c915
Ivan Chernyshov <ivan-chernyshoff@yandex.ru>
2024-09-25 00:53:51 +0300

    Updates compound data file and related functionality

--- nistchempy/compound_list.py
+++ nistchempy/compound_list.py
@@ -21,16 +21,12 @@ def get_all_data() -> _pd.core.frame.DataFrame:
         _pd.core.frame.DataFrame: dataframe containing pre-extracted compound info
     
     '''
-    dt0 = {'mol_weight': 'float64'}
-    dt1 = {k: 'string' for k in ('ID', 'name', 'formula', 'inchi', 'inchi_key', 'cas_rn')}
-    dt2 = {k: 'bool' for k in ('mol2D', 'mol3D', 'cIR', 'cTZ', 'cMS', 'cUV', 'cGC',
-                               'cTG', 'cTC', 'cTP', 'cSO', 'cTR', 'cIE', 'cIC', 'cES', 'cDI')}
-    dtypes = {**dt0, **dt1, **dt2}
     pkg = _importlib_resources.files('nistchempy')
     data_file = pkg / 'nist_data.zip'
     with _importlib_resources.as_file(data_file) as path:
         zf = _zipfile.ZipFile(path) 
-        df = _pd.read_csv(zf.open('nist_data.csv'), dtype = dtypes)
+        df = _pd.read_csv(zf.open('nist_data.csv'), dtype = 'str')
+        df['mol_weight'] = df['mol_weight'].astype(float)
         zf.close()
     
     return df
