41e0ccd0eedc0a12d22e8f6eac9c6f5652a7dbc8
Ivan Chernyshov <ivan-chernyshoff@yandex.ru>
2024-09-25 00:53:51 +0300

    Updates compound data file and related functionality

--- nistchempy/__init__.py
+++ nistchempy/__init__.py
@@ -12,6 +12,6 @@ __license__ = 'MIT'
 from nistchempy.compound_list import get_all_data
 from nistchempy.compound import get_compound
 from nistchempy.search import run_search, NistSearchParameters
-from nistchempy.search import print_search_parameters
+from nistchempy.search import get_search_parameters, print_search_parameters
 
 
