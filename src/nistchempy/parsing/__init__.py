f9fb58ab00d7b5f1b38ce20aee44e05e5027408f
Ivan Chernyshov <ivan-chernyshoff@yandex.ru>
2025-05-05 00:29:22 +0300

    Adds gas chromatography (untested)

--- src/nistchempy/parsing/__init__.py
+++ src/nistchempy/parsing/__init__.py
@@ -2,5 +2,6 @@
 
 from nistchempy.parsing.compound import is_compound_page, get_found_compounds, \
                                         parse_compound_page
-
+from nistchempy.parsing.gas_chromatography import get_chromatography_table_refs, \
+                                                  parse_chromatography_table
 
